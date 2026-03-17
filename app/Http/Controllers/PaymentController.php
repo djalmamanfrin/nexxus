d4cb5c0f701731327176fe5a5facfeb138598312
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-03-17 16:54:05 -0300

    feat: criar controller de listagem de pagamentos com filtros e suporte ao Inertia

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Expense;
+use App\Models\Payment;
+use App\Models\PaymentStatus;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class PaymentController extends Controller
+{
+    public function index(Request $request)
+    {
+        $payments = Payment::filter($request->all())
+            ->latest()
+            ->paginate(10)
+            ->withQueryString();
+
+        return Inertia::render('payments/Index', [
+            'payments' => $payments,
+            'statuses' => PaymentStatus::select('id as value', 'name as label')->get(),
+            'search_by' => $request->search_by,
+            'status' => $request->status,
+            'paid_at' => $request->paid_at,
+            'created_to' => $request->created_to,
+        ]);
+    }
+}
