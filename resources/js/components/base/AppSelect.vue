49a5d5bffaf04318f3c8574825b87d570ac64558
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-03-20 13:25:50 -0300

    refactor: remover classe desnecessária no container do `AppSelect`

--- resources/js/components/base/AppSelect.vue
+++ resources/js/components/base/AppSelect.vue
@@ -38,7 +38,7 @@ const handleCreated = (item) => {
 </script>
 
 <template>
-    <div class="flex w-full flex-col gap-1" :class="width">
+    <div class="flex w-full flex-col" :class="width">
         <div class="flex items-center justify-between">
             <label v-if="label" class="text-xs font-medium text-gray-600">
                 {{ label }}
