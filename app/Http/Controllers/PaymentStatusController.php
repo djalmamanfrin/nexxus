ba13d4a203c849ec1b0b53f3e5f3be20c2f749fa
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-03-20 09:10:41 -0300

    refactor: alterar retorno do método `store` em `PaymentStatusController` para JSON

--- app/Http/Controllers/PaymentStatusController.php
+++ app/Http/Controllers/PaymentStatusController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePaymentStatusRequest;
 use App\Models\PaymentStatus;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class PaymentStatusController extends Controller
 {
-    public function store(StorePaymentStatusRequest $request): RedirectResponse
+    public function store(StorePaymentStatusRequest $request): JsonResponse
     {
-        PaymentStatus::create($request->validated());
-        return back()->with('success', 'Status de pagamento criado!');
+        $paymentStatus = PaymentStatus::create($request->validated());
+        return response()->json($paymentStatus->only('id', 'name', 'code'));
     }
 }
