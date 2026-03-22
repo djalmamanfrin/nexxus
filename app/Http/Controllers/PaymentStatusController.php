a2ef25ee327ee081ceecdb002d598af6351d4f85
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-03-22 09:43:40 -0300

    refactor: adicionar endpoint `index` para listar status de pagamento

--- app/Http/Controllers/PaymentStatusController.php
+++ app/Http/Controllers/PaymentStatusController.php
@@ -5,10 +5,19 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StorePaymentStatusRequest;
 use App\Models\PaymentStatus;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class PaymentStatusController extends Controller
 {
+    public function index(Request $request): JsonResponse
+    {
+        $paymentStatus = PaymentStatus::query()
+            ->select('id as value', 'name as label')
+            ->get();
+
+        return response()->json($paymentStatus);
+    }
     public function store(StorePaymentStatusRequest $request): JsonResponse
     {
         $paymentStatus = PaymentStatus::create($request->validated());
