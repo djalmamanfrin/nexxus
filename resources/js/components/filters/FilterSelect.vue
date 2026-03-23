fb27ef4065f61b72f58bfe027ff6cbc019793cf7
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-03-22 21:59:35 -0300

    refactor: adicionar controle de seleção com `handleSelected` no componente `FilterSelect`

--- resources/js/components/filters/FilterSelect.vue
+++ resources/js/components/filters/FilterSelect.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { inject, onMounted } from 'vue';
+import { inject, ref, onMounted } from 'vue';
 import AppSelect from '@/components/base/AppSelect.vue';
 
 const filters = inject('filters');
@@ -11,7 +11,7 @@ const props = defineProps({
     options: Array,
     width: String,
 });
-
+// verificar necessidade de onMounted depois
 onMounted(() => {
     registerFilter?.(props.name, {
         label: 'com status',
@@ -19,12 +19,25 @@ onMounted(() => {
         options: props.options,
     });
 });
+
+const selectedItem = ref(null);
+const handleSelected = (item) => {
+    selectedItem.value = item;
+
+    registerFilter?.(props.name, {
+        label: 'com status',
+        type: 'select',
+        value: item.value,
+        display: item.label,
+    });
+};
 </script>
 
 <template>
     <AppSelect
         class="min-w-[180px] flex-[1] md:max-w-[250px]"
         v-model="filters[name]"
+        @selected="handleSelected"
         url="/payment-statuses"
         :label="label"
         :width="width"
