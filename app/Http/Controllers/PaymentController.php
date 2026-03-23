9884b1bbd8ba313adb19c39b2be98ada4c2ca0cd
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-03-23 20:48:16 -0300

    refactor: consolidar lógica de atualização e validação de pagamentos no controlador

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,12 +5,11 @@ namespace App\Http\Controllers;
 use App\Actions\Attachment\AttachFileAction;
 use App\Models\Payment;
 use App\Models\PaymentStatus;
-use App\Models\PaymentType;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
-use Inertia\Response;
+use Throwable;
 
 class PaymentController extends Controller
 {
@@ -32,7 +31,7 @@ class PaymentController extends Controller
     }
 
     /**
-     * @throws \Throwable
+     * @throws Throwable
      */
     public function store(Request $request, AttachFileAction $action): RedirectResponse
     {
@@ -48,11 +47,33 @@ class PaymentController extends Controller
         return back()->with('success', 'Pagamento criado!');
     }
 
-    public function edit(Payment $payment): Response
+    /**
+     * @throws Throwable
+     */
+    public function update(Request $request, Payment $payment, AttachFileAction $attachFile): RedirectResponse
     {
-        return Inertia::render('payments/Edit', [
-            'payment' => $payment->with('attachments')->first(),
-            'statuses' => PaymentStatus::select('id as value', 'name as label')->get(),
+        $validated = $request->validate([
+            'expense_id' => ['nullable', 'integer'],
+            'bank_account_id' => ['nullable', 'integer'],
+            'payment_status_id' => ['nullable', 'integer'],
+            'payment_type_id' => ['nullable', 'integer'],
+            'amount' => ['nullable', 'numeric'],
+            'paid_at' => ['nullable', 'date'],
+            'attachment' => ['nullable', 'file', 'mimes:jpg,jpeg,png,webp'],
         ]);
+
+        DB::transaction(function () use ($validated, $request, $payment, $attachFile) {
+            $payment->update($validated);
+            if ($request->hasFile('attachment')) {
+                $payment->attachments()->delete();
+                $attachFile->execute(
+                    $payment,
+                    $request->file('attachment'),
+                    'payments'
+                );
+            }
+        });
+
+        return back()->with('success', 'Atualizado com sucesso');
     }
 }
