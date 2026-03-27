d8c163dfb9c53090118da9e5a78626fa23631284
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-03-27 17:26:07 -0300

    feat: adicionar tipo de centro de custo "Outros" no seeder

--- database/seeders/CostCenterTypeSeeder.php
+++ database/seeders/CostCenterTypeSeeder.php
@@ -10,6 +10,7 @@ class CostCenterTypeSeeder extends Seeder
     public function run(): void
     {
         CostCenterType::insert([
+            ['name' => 'Outros', 'code' => 'OUT'],
             ['name' => 'Operational', 'code' => 'OP'],
             ['name' => 'Administrative', 'code' => 'ADM'],
             ['name' => 'Investment', 'code' => 'INV'],
