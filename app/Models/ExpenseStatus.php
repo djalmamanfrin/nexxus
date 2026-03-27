e48d078bc0cdaa44939c34a2d843529a91bab0d7
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-03-27 11:59:35 -0300

    docs: adicionar PHPDoc ao modelo `ExpenseStatus`

--- app/Models/ExpenseStatus.php
+++ app/Models/ExpenseStatus.php
@@ -5,6 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property string $color
+ * @property int $active
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Expense> $expenses
+ * @property-read int|null $expenses_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ExpenseStatus newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ExpenseStatus newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ExpenseStatus query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ExpenseStatus whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ExpenseStatus whereColor($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ExpenseStatus whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ExpenseStatus whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ExpenseStatus whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ExpenseStatus whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ExpenseStatus whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class ExpenseStatus extends Model
 {
     const PENDING = 1;
