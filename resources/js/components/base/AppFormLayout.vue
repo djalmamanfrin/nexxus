ed68f8cb8585a50c421b3e17bfedf72d5ca4e5ad
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-03-28 18:26:52 -0300

    feat: adicionar componente AppFormLayout para padronizar layout de formulários

---
+++ resources/js/components/base/AppFormLayout.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+    item: any;
+}>();
+</script>
+
+<template>
+    <div v-if="item" class="space-y-4">
+        <div class="grid grid-cols-1 gap-4">
+            <slot />
+        </div>
+
+        <div class="flex items-center justify-between pt-2">
+            <slot name="footer" />
+        </div>
+    </div>
+</template>
