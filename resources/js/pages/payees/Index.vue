c6d6623975ce469f5b96a01f6b74d47d4e66b0eb
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-03-29 16:53:21 -0300

    refactor: ajustar endpoint em CreatePayee.vue para criação de payees

--- resources/js/pages/payees/Index.vue
+++ resources/js/pages/payees/Index.vue
@@ -5,9 +5,10 @@ import { useFilters } from '@/composables/useFilters';
 import CrudIndexPage from '@/pages/CrudIndexPage.vue';
 import AppFormLayout from '@/components/base/AppFormLayout.vue';
 import AppInput from '@/components/base/AppInput.vue';
-import AppTextarea from '@/components/base/AppTextarea.vue';
 import AppSwitch from '@/components/base/AppSwitch.vue';
-import FilterTabs from '@/components/filters/FilterTabs.vue';
+import AppLayout from '@/layouts/AppLayout.vue';
+import AppButtonWithModal from '@/components/base/AppButtonWithModal.vue';
+import CreatePayee from '@/pages/payees/CreatePayee.vue';
 
 const props = defineProps<{
     payees: {
@@ -36,87 +37,93 @@ const columns = [
     { key: 'created_at', label: 'Criado em', type: 'datetime' },
 ];
 
-const breadcrumbItems: BreadcrumbItem[] = [
+const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Beneficiário',
         href: '',
-        btn: { label: 'Novo Beneficiário', url: url },
     },
 ];
 </script>
 
 <template>
-    <CrudIndexPage
-        :items="props.payees"
-        :columns="columns"
-        :base-url="url"
-        :breadcrumbs="breadcrumbItems"
-        v-model:filters="filters"
-        :search="search"
-        :clear="clear"
-        :initialForm="{
-            name: null,
-            document: null,
-            document_type: null,
-            is_pix_document: null,
-            pix_key: null,
-            pix_key_type: null,
-        }"
-        :mapToForm="
-            (item) => ({
-                name: item.name,
-                document: item.document,
-                document_type: item.document_type,
-                is_pix_document: item.is_pix_document,
-                pix_key: item.pix_key,
-                pix_key_type: item.pix_key_type,
-            })
-        "
-    >
-        <template #filters>
-            <FilterText
-                label="Buscar Beneficiário"
-                name="search_by"
-                placeholder="Pesquise pelo nome ou CPF, CNPJ"
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <template #header-actions>
+            <AppButtonWithModal
+                :create-component="CreatePayee"
+                label="Novo Beneficiário"
             />
         </template>
+        <CrudIndexPage
+            :items="props.payees"
+            :columns="columns"
+            :base-url="url"
+            v-model:filters="filters"
+            :search="search"
+            :clear="clear"
+            :initialForm="{
+                name: null,
+                document: null,
+                document_type: null,
+                is_pix_document: null,
+                pix_key: null,
+                pix_key_type: null,
+            }"
+            :mapToForm="
+                (item) => ({
+                    name: item.name,
+                    document: item.document,
+                    document_type: item.document_type,
+                    is_pix_document: item.is_pix_document,
+                    pix_key: item.pix_key,
+                    pix_key_type: item.pix_key_type,
+                })
+            "
+        >
+            <template #filters>
+                <FilterText
+                    label="Buscar Beneficiário"
+                    name="search_by"
+                    placeholder="Pesquise pelo nome ou CPF, CNPJ"
+                />
+            </template>
 
-        <template #title> Beneficiário </template>
+            <template #title> Beneficiário </template>
 
-        <template #form="{ item, form }">
-            <AppFormLayout :item="item">
-                <AppInput
-                    v-model="form.name"
-                    :error="form.errors.name"
-                    label="Name"
-                />
-                <AppInput
-                    v-model="form.document_type"
-                    :error="form.errors.document_type"
-                    label="Tipo de documento"
-                />
-                <AppInput
-                    v-model="form.document"
-                    :error="form.errors.document"
-                    label="CNPJ/CPF"
-                />
-                <AppSwitch
-                    v-model="form.is_pix_document"
-                    label="Usar documento como PIX"
-                />
-                <div v-if="!form.is_pix_document">
+            <template #form="{ item, form }">
+                <AppFormLayout :item="item">
                     <AppInput
-                        v-model="form.pix_key_type"
-                        label="Tipo de chave Pix"
-                        :error="form.errors.pix_key_type"
+                        v-model="form.name"
+                        :error="form.errors.name"
+                        label="Name"
                     />
                     <AppInput
-                        v-model="form.pix_key"
-                        label="Chave pix"
-                        :error="form.errors.pix_key"
+                        v-model="form.document_type"
+                        :error="form.errors.document_type"
+                        label="Tipo de documento"
                     />
-                </div>
-            </AppFormLayout>
-        </template>
-    </CrudIndexPage>
+                    <AppInput
+                        v-model="form.document"
+                        :error="form.errors.document"
+                        label="CNPJ/CPF"
+                    />
+                    <AppSwitch
+                        v-model="form.is_pix_document"
+                        label="Usar documento como PIX"
+                    />
+                    <div v-if="!form.is_pix_document">
+                        <AppInput
+                            v-model="form.pix_key_type"
+                            label="Tipo de chave Pix"
+                            :error="form.errors.pix_key_type"
+                        />
+                        <AppInput
+                            v-model="form.pix_key"
+                            label="Chave pix"
+                            :error="form.errors.pix_key"
+                        />
+                    </div>
+                </AppFormLayout>
+            </template>
+        </CrudIndexPage>
+    </AppLayout>
 </template>
