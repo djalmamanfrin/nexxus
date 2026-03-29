46a1936220118effab972d2b1840115f6dd43f38
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-03-29 10:08:09 -0300

    feat: adicionar UpdateCostCenterRequest com validações para atualização de centros de custos

---
+++ app/Http/Requests/UpdateCostCenterRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCostCenterRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'work_id' => ['required', 'integer'],
+            'code' => ['required', 'string'],
+            'description' => ['required', 'string'],
+            'budget' => ['nullable', 'numeric'],
+            'start_date' => ['nullable', 'date'],
+            'expected_end_date' => ['nullable', 'date'],
+        ];
+    }
+}
