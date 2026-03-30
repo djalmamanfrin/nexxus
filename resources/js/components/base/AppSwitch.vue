13b5410deb15897fce3bd497a36c50ef71e3a8ed
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-03-30 17:08:08 -0300

    refactor: adicionar suporte a mensagens de erro e simplificar estrutura do AppSwitch.vue

--- resources/js/components/base/AppSwitch.vue
+++ resources/js/components/base/AppSwitch.vue
@@ -7,6 +7,10 @@ const props = defineProps({
         type: Boolean,
         default: false,
     },
+    error: {
+        type: String,
+        default: '',
+    },
     disabled: Boolean,
 });
 
@@ -19,33 +23,33 @@ const toggle = () => {
 </script>
 
 <template>
-    <div
-        @click="toggle"
-        :class="[
-            'flex w-full items-center justify-between rounded-md border px-3 py-2 text-sm transition',
-            'border-gray-300 bg-white dark:bg-gray-950',
-            !disabled && 'cursor-pointer hover:border-indigo-400',
-            disabled && 'opacity-50 cursor-not-allowed'
-        ]"
-    >
-        <!-- Label -->
-        <span class="text-sm text-gray-700 dark:text-gray-200">
-            {{ label }}
-        </span>
-
-        <!-- Toggle -->
+    <div class="flex w-full flex-col gap-1">
         <div
+            @click="toggle"
             :class="[
-                'relative inline-flex h-5 w-10 items-center rounded-full transition',
-                modelValue ? 'bg-indigo-600' : 'bg-gray-300'
+                'flex w-full items-center justify-between rounded-md border px-3 py-2 text-sm transition',
+                'border-gray-300 bg-white dark:bg-gray-950',
+                !disabled && 'cursor-pointer hover:border-indigo-400',
+                disabled && 'cursor-not-allowed opacity-50',
             ]"
         >
-            <span
+            <AppLabel v-if="label" :label="label" />
+            <div
                 :class="[
-                    'inline-block h-4 w-4 transform rounded-full bg-white transition',
-                    modelValue ? 'translate-x-5' : 'translate-x-1'
+                    'relative inline-flex h-5 w-10 items-center rounded-full transition',
+                    modelValue ? 'bg-indigo-600' : 'bg-gray-300',
                 ]"
-            />
+            >
+                <span
+                    :class="[
+                        'inline-block h-4 w-4 transform rounded-full bg-white transition',
+                        modelValue ? 'translate-x-5' : 'translate-x-1',
+                    ]"
+                />
+            </div>
         </div>
+        <span v-if="error" class="text-xs text-red-500">
+            {{ error }}
+        </span>
     </div>
 </template>
