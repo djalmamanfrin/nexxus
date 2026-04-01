91004b213e9752d2aaf452293cd6158579168333
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-01 16:14:15 -0300

    refactor: tipar métodos de DocumentCast e ajustar validação de valores

--- app/Casts/DocumentCast.php
+++ app/Casts/DocumentCast.php
@@ -4,18 +4,20 @@ namespace App\Casts;
 
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use App\Domain\Documents\DocumentFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Stringable;
 
 class DocumentCast implements CastsAttributes
 {
-    public function get($model, $key, $value, $attributes)
+    public function get(Model $model, string $key, mixed $value, array $attributes): mixed
     {
         return DocumentFactory::make($value);
     }
 
-    public function set($model, $key, $value, $attributes)
+    public function set(Model $model, string $key, mixed $value, array $attributes): mixed
     {
-        return $value instanceof \Stringable
+        return $value instanceof Stringable
             ? $value->value()
-            : preg_replace('/\D/', '', $value);
+            : $value;
     }
 }
