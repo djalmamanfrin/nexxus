b421d44a7b35f3e7283fe6e6876aaffd40506df8
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-03 15:47:51 -0300

    refactor: remover importação não utilizada de PixKeyInterface em PayeeResource

--- app/Http/Resources/PayeeResource.php
+++ app/Http/Resources/PayeeResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources;
 
-use App\Domain\PixKey\PixKeyInterface;
 use App\Domain\ValidatorCastInterface;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
