f32b3bb998195a0c93be914dd3d2da86dba0e2e0
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-03 18:51:24 -0300

    refactor: ajustar layout em AppFormLayout.vue para exibir campos obrigatórios e reorganizar slots

--- resources/js/components/base/AppFormLayout.vue
+++ resources/js/components/base/AppFormLayout.vue
@@ -9,15 +9,18 @@ defineProps<{
 
 <template>
     <div v-if="item" class="space-y-4">
-        <div class="grid grid-cols-1 gap-4">
-            <slot />
-        </div>
-
         <div class="flex items-center justify-between pt-2">
             <AppLabel label="Criado em:" />
             <p class="text-sm text-gray-500">
                 {{ formatDateTime(item.created_at) }}
             </p>
+        </div>
+        <div class="grid grid-cols-1 gap-4">
+            <slot />
+        </div>
+
+        <div class="flex items-center justify-between pt-2">
+            <AppLabel label="Campos obrigatórios " :required="true" class="text-xs" />
         </div>
     </div>
 </template>
