4d4c98dccfe98f711c075881eccd601b40df1798
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-03 23:45:24 -0300

    feat: adicionar componente AppBadge para exibição de etiquetas coloridas

---
+++ resources/js/components/base/AppBadge.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+    label: String,
+    color: String,
+});
+
+const map = {
+    yellow: 'bg-yellow-100 text-yellow-800',
+    green: 'bg-green-100 text-green-800',
+    red: 'bg-red-100 text-red-800',
+};
+</script>
+
+<template>
+    <span
+        :class="[
+            'rounded px-2 py-1 text-xs',
+            map[color] || 'bg-gray-100 text-gray-800',
+        ]"
+    >
+        {{ label }}
+    </span>
+</template>
