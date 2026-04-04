aa158936766017f2dc3ef36d862a6b9f049e9c46
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-04 15:26:07 -0300

    refactor: adicionar migration para criação da tabela cost_center_statuses

---
+++ database/migrations/2026_03_16_094325_create_cost_center_statuses_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('cost_center_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Ex: Pending, Paid, Cancelled
+            $table->string('slug')->unique(); // Ex: pending, paid, cancelled
+            $table->string('color')->default('gray'); // usado em UI (badge, label)
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('cost_center_statuses');
+    }
+};
