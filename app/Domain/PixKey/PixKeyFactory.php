ec0971ec10aef2b518bf4515ba4a1fd15c229f54
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-04 01:59:46 -0300

    feat: adicionar logging ao validar PixKey e ajustar ordem de candidatos

--- app/Domain/PixKey/PixKeyFactory.php
+++ app/Domain/PixKey/PixKeyFactory.php
@@ -3,19 +3,21 @@
 namespace App\Domain\PixKey;
 
 use App\Domain\ValidatorInterface;
+use App\Support\Logger;
 use Illuminate\Validation\ValidationException;
 
 class PixKeyFactory
 {
     private static array $candidates = [
-        PixEmail::class,
-        PixPhone::class,
         PixCpf::class,
         PixCnpj::class,
+        PixEmail::class,
+        PixPhone::class, // fone tem que ser depois de cpf pq ambos tem o mesmo lenght
     ];
     public static function make(string $value): ValidatorInterface
     {
         foreach (self::$candidates as $class) {
+            Logger::info("Validando PixKey {$value} com {$class}");
             if ($class::matches($value)) {
                 return new $class($value);
             }
