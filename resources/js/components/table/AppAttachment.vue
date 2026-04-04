240190752a842372a9df24cc758d91f63ce9dad7
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-04 00:40:52 -0300

    refactor: substituir AppBadge por AppAttachment com suporte a lista de anexos

---
+++ resources/js/components/table/AppAttachment.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Attachment } from '@/types/index.d.ts';
+
+const props = defineProps<{
+    attachments?: Attachment[];
+}>();
+
+</script>
+
+<template>
+    <span v-if="props.attachments?.length">
+        {{ props.attachments[0].original_name }}
+    </span>
+    <span v-else>-</span>
+</template>
