84b917d94ccf011590e868275f1a09784ff21f0e
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-05 17:22:05 -0300

    chore: adicionar componente TypeFields para gerenciamento de campos de tipo em centros de custo

---
+++ resources/js/pages/cost_centers/TypeFields.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AppInput from '@/components/base/AppInput.vue';
+import AppTextarea from '@/components/base/AppTextarea.vue';
+import AppSwitch from '@/components/base/AppSwitch.vue';
+
+defineProps<{
+    form: any;
+}>();
+</script>
+
+<template>
+    <AppInput v-model="form.name" :error="form.errors.name" label="Nome" />
+    <AppInput v-model="form.code" :error="form.errors.code" label="Código" />
+    <AppSwitch v-model="form.is_active" label="Ativo" />
+    <AppTextarea
+        v-model="form.description"
+        :error="form.errors.description"
+        label="Descrição"
+        placeholder="Uma descrição que ajuda a identificar o centro de custo"
+    />
+</template>
