8f05d320e43b5b12a23021485d413d698bda8fe3
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-05 15:49:54 -0300

    chore: remover linha em branco desnecessária no AppTooltip.vue

--- resources/js/components/base/AppTooltip.vue
+++ resources/js/components/base/AppTooltip.vue
@@ -10,7 +10,6 @@ defineProps<{
 <template>
     <div v-if="tooltip" class="group/icon relative cursor-pointer">
         <Icon v-if="icon" :name="icon" />
-
         <div
             class="pointer-events-none absolute right-0 top-6 z-10 w-56 scale-95 rounded-lg bg-gray-900 p-2 text-xs text-white opacity-0 shadow-lg transition-all duration-200 group-hover/icon:scale-100 group-hover/icon:opacity-100"
         >
