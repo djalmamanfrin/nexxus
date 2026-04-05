7788aae8cf76825e3a66deb7925ae60466978bd5
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-05 15:29:26 -0300

    chore: criar componente reutilizável AppTooltip para exibir ícones com dicas visuais

---
+++ resources/js/components/base/AppTooltip.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Icon from '@/components/Icon.vue';
+
+defineProps<{
+    icon?: string;
+    tooltip?: string;
+}>();
+</script>
+
+<template>
+    <div v-if="tooltip" class="group/icon relative cursor-pointer">
+        <Icon v-if="icon" :name="icon" />
+
+        <div
+            class="pointer-events-none absolute left-1/2 top-6 z-10 w-56 -translate-x-1/2 scale-95 rounded-lg bg-gray-900 p-2 text-xs text-white opacity-0 shadow-lg transition-all duration-200 group-hover/icon:scale-100 group-hover/icon:opacity-100"
+        >
+            {{ tooltip }}
+        </div>
+    </div>
+</template>
