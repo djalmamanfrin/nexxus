bf4e6e3870d910148d096954353a99e6ff79231c
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-05 02:49:48 -0300

    chore: criar componentes de gráficos (BarChart, LineChart, PieChart) com vue-chartjs

---
+++ resources/js/components/charts/BarChart.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Bar } from 'vue-chartjs';
+
+defineProps<{
+    data: any;
+    options?: any;
+}>();
+</script>
+
+<template>
+    <Bar :data="data" :options="options" />
+</template>
---
+++ resources/js/components/charts/PieChart.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Pie } from 'vue-chartjs';
+
+defineProps<{
+    data: any;
+    options?: any;
+}>();
+</script>
+
+<template>
+    <Pie :data="data" :options="options" />
+</template>
