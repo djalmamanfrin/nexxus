912586709efdb9caac80d99e0183c139ead21e29
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-05 17:59:32 -0300

    chore: criar WorkObserver e associar tipos de centro de custo ao criar Work

---
+++ app/Observers/WorkObserver.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Observers;
+
+namespace App\Observers;
+
+use App\Models\CostCenter;
+use App\Models\CostCenterType;
+use App\Models\Work;
+
+class WorkObserver
+{
+    public function created(Work $work): void
+    {
+        $types = CostCenterType::where('active', true)
+            ->select('id')
+            ->get();
+
+        foreach ($types as $type) {
+            CostCenter::associateWorkAndType($work->id, $type->id);
+        }
+    }
+}
