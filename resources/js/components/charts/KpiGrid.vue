109c56e3f10a8720358fc68dd1b11dde52e04a0c
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-05 15:45:30 -0300

    chore: ajustar KpiGrid para suportar 2 colunas em telas médias e 4 em telas grandes

--- resources/js/components/charts/KpiGrid.vue
+++ resources/js/components/charts/KpiGrid.vue
@@ -11,7 +11,7 @@ defineProps<{
 </script>
 
 <template>
-    <div class="grid grid-cols-1 gap-4 md:grid-cols-4">
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
         <KpiCard
             v-for="(item, index) in items"
             :key="index"
