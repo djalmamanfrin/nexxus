6db0bf948a1aa574d9f258933b0b331c64889723
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-08 16:07:03 -0300

    chore: adicionar componente `FilterMultiSelect.vue` para suporte a seleção múltipla em filtros

---
+++ resources/js/components/filters/FilterMultiSelect.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import AppMultiSelect from '@/components/base/AppMultiSelect.vue';
+import { SelectOption } from '@/types/select';
+import { inject, onMounted, ref, watch } from 'vue';
+
+const filters = inject('filters');
+const registerFilter = inject('registerFilter');
+
+const props = defineProps<{
+    label?: string;
+    name: string;
+    options: SelectOption[];
+    width?: string;
+}>();
+
+const selectedItems = ref<SelectOption[]>([]);
+
+watch(
+    () => filters[props.name],
+    (values) => {
+        if (!values?.length) {
+            selectedItems.value = [];
+            return;
+        }
+
+        selectedItems.value = props.options.filter((opt) =>
+            values.includes(opt.value),
+        );
+    },
+    { immediate: true },
+);
+
+const handleSelected = (items: SelectOption[]) => {
+    selectedItems.value = items;
+
+    filters[props.name] = items.map((item) => item.value);
+
+    registerFilter?.(props.name, {
+        label: 'em empresas',
+        type: 'multi-select',
+        value: items.map((i) => i.value),
+        display: items.map((i) => i.label).join(', '),
+    });
+};
+
+onMounted(() => {
+    if (!filters[props.name]?.length) return;
+
+    handleSelected(
+        props.options.filter((opt) => filters[props.name].includes(opt.value)),
+    );
+});
+</script>
+
+<template>
+    <AppMultiSelect
+        :label="label"
+        :options="options"
+        :model-value="filters[name]"
+        @update:modelValue="handleSelected"
+        :width="width"
+    />
+</template>
