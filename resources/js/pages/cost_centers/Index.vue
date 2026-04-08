db5890823bdda6b28f6cafa92cb95ffb72d0f2cc
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-08 16:06:32 -0300

    chore: ajustar colunas na tabela de `cost_centers/Index.vue`

--- resources/js/pages/cost_centers/Index.vue
+++ resources/js/pages/cost_centers/Index.vue
@@ -35,13 +35,14 @@ const props = defineProps<{
 const columns = [
     { key: 'work.name', label: 'Obra', align: 'left' },
     { key: 'type.code', label: 'Código', type: 'badge' },
-    { key: 'budget.formatted', label: 'Orçamento' },
+    { key: 'type.name', label: 'Tipo' },
     {
         key: 'status.name',
         label: 'Status',
         type: 'badge',
         color: 'status.color',
     },
+    { key: 'budget.formatted', label: 'Orçamento' },
     { key: 'start_date.formatted', label: 'Início' },
     { key: 'expected_end_date.formatted', label: 'Término' },
     { key: 'created_at.formatted', label: 'Criado em' },
