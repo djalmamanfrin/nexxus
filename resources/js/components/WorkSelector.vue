9f83978c9e4668182eb08b45f16a3e5d801d66ec
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-11 12:20:15 -0300

    chore: adicionar `Tooltip` no `WorkSelector` para estado colapsado ou mobile

--- resources/js/components/WorkSelector.vue
+++ resources/js/components/WorkSelector.vue
@@ -3,14 +3,24 @@ import { Link, router, usePage } from '@inertiajs/vue3';
 import AppButton from '@/components/AppButton.vue';
 import axios from 'axios';
 import { SelectOption } from '@/types/select';
-import { onMounted, ref } from 'vue';
+import { type Component, onMounted, ref } from 'vue';
 import { Building2Icon } from 'lucide-vue-next';
 import { dashboard } from '@/routes';
 import AppLogo from '@/components/AppLogo.vue';
+import { useSidebar } from '@/components/ui/sidebar';
+import SidebarMenuButtonChild from '@/components/ui/sidebar/SidebarMenuButtonChild.vue';
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from '@/components/ui/tooltip';
+import Icon from '@/components/Icon.vue';
 
 const page = usePage();
 const activeWorkId = page.props.auth.user.active_work_id;
 const emit = defineEmits(['update:modelValue']);
+const { isMobile, state } = useSidebar();
 
 function handleChange(workId: string | number | null) {
     emit('update:modelValue', workId);
@@ -48,7 +58,24 @@ onMounted(fetchOptions);
 </script>
 
 <template>
+    <TooltipProvider v-if="state === 'collapsed' || isMobile" :delay-duration="0">
+        <Tooltip>
+            <TooltipTrigger>
+                <AppButton
+                    type="link"
+                    href="/works"
+                    variant="secondary"
+                    :icon="Building2Icon"
+                    class="p-2"
+                />
+            </TooltipTrigger>
+            <TooltipContent side="right" align="center">
+                {{ workSelectorName }}
+            </TooltipContent>
+        </Tooltip>
+    </TooltipProvider>
     <AppButton
+        v-else
         type="link"
         href="/works"
         variant="secondary"
