bd39be3854c179f0b3c5f92bf140f4600cf01e56
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-11 22:04:17 -0300

    chore: criar componente ColumnSection para exibição de colunas na interface de conciliação

---
+++ resources/js/components/reconciliation/ColumnSection.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+    title: string;
+}>();
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <!-- 🏷️ HEADER -->
+        <div
+            class="mb-3 rounded-lg uppercase border border-gray-200 bg-gray-50 px-3 py-2 text-center text-sm font-semibold text-gray-700 shadow-sm dark:border-neutral-800 dark:bg-neutral-900 dark:text-gray-200"
+        >
+            {{ title }}
+        </div>
+
+        <div class="flex flex-col">
+            <slot />
+        </div>
+    </div>
+</template>
