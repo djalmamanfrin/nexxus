f9eeb8fb04cf1866eccd6de01f1b0761e5305b73
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-13 08:38:02 -0300

    chore: adicionar novos status e atualizar atributos em seeders de ExpenseStatus e PaymentStatus

--- database/seeders/ExpenseStatusSeeder.php
+++ database/seeders/ExpenseStatusSeeder.php
@@ -10,8 +10,10 @@ class ExpenseStatusSeeder extends Seeder
     public function run(): void
     {
         collect([
-            ['name' => 'Pendente', 'slug' => 'pendente', 'color' => 'yellow'],
-            ['name' => 'Concluído', 'slug' => 'done', 'color' => 'green'],
+            ['name' => 'Pendente', 'slug' => 'pendente', 'color' => 'gray', 'sort_order' => 1],
+            ['name' => 'Parcial', 'slug' => 'parcial', 'color' => 'yellow', 'sort_order' => 2],
+            ['name' => 'Conciliado', 'slug' => 'conciliado', 'color' => 'green', 'sort_order' => 3],
+            ['name' => 'Excedente', 'slug' => 'excedente', 'color' => 'orange', 'sort_order' => 4],
         ])->each(fn ($status) => ExpenseStatus::create($status));
     }
 }
