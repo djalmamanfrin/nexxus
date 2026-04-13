8b852b36b68dd4dac830a6cc9ab71d0716f8e127
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-13 16:37:30 -0300

    chore: adicionar componente AppBadge e suporte a cores no FieldRow.vue

--- resources/js/components/reconciliation/FieldRow.vue
+++ resources/js/components/reconciliation/FieldRow.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import AppBadge from '@/components/table/AppBadge.vue';
+
 defineProps<{
     label: string;
-    value?: string | number | null;
+    value?: string;
+    color?: string;
     mono?: boolean;
 }>();
 </script>
@@ -10,7 +13,8 @@ defineProps<{
     <div class="flex items-center justify-between">
         <span class="text-gray-500">{{ label }}</span>
 
-        <span
+        <AppBadge v-if="color && value" :label="value" :color="color" />
+        <span v-else
             :class="[
                 'font-medium text-gray-800 dark:text-gray-200',
                 mono ? 'font-mono text-gray-700 dark:text-gray-300' : '',
