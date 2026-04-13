db278c6147731be63d12b41d55700be55d266b80
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-13 17:06:51 -0300

    chore: tornar título opcional e exibir condicionalmente em Section.vue

--- resources/js/components/reconciliation/Section.vue
+++ resources/js/components/reconciliation/Section.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 defineProps<{
-    title: string;
+    title?: string;
 }>();
 </script>
 
 <template>
     <div class="flex flex-col gap-2 text-sm">
-        <span class="text-xs font-medium tracking-wide text-gray-400 uppercase">
+        <span v-if="title" class="text-xs font-medium tracking-wide text-gray-400 uppercase">
             {{ title }}
         </span>
 
