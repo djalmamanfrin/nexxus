c9d85f7b6b41fe75737d12214da95d1252008e31
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-13 17:06:35 -0300

    chore: adicionar suporte a tema escuro e nova cor 'gray' no AppBadge.vue

--- resources/js/components/table/AppBadge.vue
+++ resources/js/components/table/AppBadge.vue
@@ -5,9 +5,10 @@ const props = defineProps({
 });
 
 const map = {
-    yellow: 'bg-yellow-100 text-yellow-800',
-    green: 'bg-green-100 text-green-800',
-    red: 'bg-red-100 text-red-800',
+    yellow: 'bg-yellow-100 text-yellow-700 dark:bg-yellow-900/40 dark:text-yellow-300',
+    green: 'bg-green-100 text-green-700 dark:bg-green-900/40 dark:text-green-300',
+    red: 'bg-red-100 text-red-700 dark:bg-red-900/40 dark:text-red-300',
+    gray: 'bg-gray-100 text-gray-700 dark:bg-gray-900/40 dark:text-gray-300',
 };
 </script>
 
